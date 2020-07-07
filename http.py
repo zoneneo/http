062f14928f9a0289185d90701744e8b61d0a0fbe
zoneneo <zoneneo@hotmail.com>
2020-07-07 22:18:06 +0800

    python自带库构成最简单的web服务

---
+++ http.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+HOST = ""
+PORT = 80
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
